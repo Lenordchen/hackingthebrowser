d9dcc5102f2a3122d986b4d8ce189470983ce223
shiiiiiyu_chen <37972678+Lenordchen@users.noreply.github.com>
2019-03-03 21:39:33 -0500

    Update background.js

--- extensions/background.js
+++ extensions/background.js
@@ -1,11 +1,26 @@
 console.log('background running!');
 
-chrome.browserAction.onClicked.addListener(buttonClicked);
+// chrome.browserAction.onClicked.addListener(buttonClicked);
+//
+// function buttonClicked(tab){
+//   let msg = {
+//     txt: "Hi I am icebear"
+//   }
+//   chrome.tabs.sendMessage(tab.id, msg);
+//   console.log(tab);
+// }
 
-function buttonClicked(tab){
-  let msg = {
-    txt: "Hi I am icebear"
+chrome.runtime.onMessage.addListener(receiver);
+
+ window.word = 'hacking the browser';
+
+function receiver (request, sender, sendResponse) {
+  console.log(request);
+  if (request.text) {
+    window.word = request.text;
+  } else if (request.fromPopup) {
+    // send the word to the popup
+    sendResponse(window.word);
   }
-  chrome.tabs.sendMessage(tab.id, msg);
-  console.log(tab);
+  window.word = request.text;
 }
