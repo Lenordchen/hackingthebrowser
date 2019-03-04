39b95adab4837366cf99de455fbe3825313231fa
shiiiiiyu_chen <37972678+Lenordchen@users.noreply.github.com>
2019-03-03 21:39:54 -0500

    Update content.js

--- extensions/content.js
+++ extensions/content.js
@@ -1,1 +1,15 @@
 console.log("run!");
+
+window.addEventListener('mouseup', mouseUpped);
+
+function mouseUpped(){
+  let selectedText = window.getSelection().toString();
+  console.log(selectedText);
+  if (selectedText.length > 0){
+    let message = {
+      text: selectedText
+    };
+    chrome.runtime.sendMessage(message);
+  }
+
+}
